8a2d1b059654d219c8ff0b806dbf76cd700198e4
PriyankaMeganathan21 <115691627+PriyankaMeganathan21@users.noreply.github.com>
2022-10-26 16:53:32 +0530

    Add files via upload

---
+++ task53e.js
@@ -0,0 +1,19 @@
+// e. Return all the palindromes in an array using arrow function
+var palindrome = (arr,N)=>{
+	var ans = [];
+	for (let i = 0; i < N; i++) {
+		if (isPalindrome(arr[i])) {
+			ans.push(arr[i]);
+		}
+	}
+	console.log(ans);
+}
+function isPalindrome(s)
+{
+	let a = s;
+	s = s.split('').reverse().join('');
+	return s == a;
+}
+let arr = [ "saippuakivikauppias", "car", "ada", "racecar", "malayalam","english" ];
+let N = arr.length;
+let s = palindrome(arr, N);
